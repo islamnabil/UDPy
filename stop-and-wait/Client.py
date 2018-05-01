e2009b439c9ca701b3d46d9e9aa09dc70547a982
Ahmed Lotfy <A7med.lotfey@gmail.com>
2018-05-01 23:26:09 +0200

    stop-and-wait Server & Client Clean up.

--- stop-and-wait/Client.py
+++ stop-and-wait/Client.py
@@ -4,53 +4,49 @@ from Server import *
 class Client:
     def __init__(self, port):
         self.ip, self.port = local_address(port)
-        self.address = (self.ip, self.port)
+        self.server_address = (self.ip, self.port)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def request(self, file):
         # Connect to server
-        self.socket.connect(self.address)
-        print('Connected to', self.address)
+        self.socket.connect(self.server_address)
+        print('Connected to', self.server_address)
 
         # Send file request
         pkt = Packet(file=file)
         self.socket.send(pkt.__dumb__())
 
         # Check for response
-        try:
-            res = self.socket.recv(PACKET_SIZE)
+        res = self.socket.recv(PACKET_SIZE)
+        if not res:
+            print('Disconnected')
+        else:
             pkt = Packet(pickled=res)
             pkt.__print__()
-
             if pkt.__get__('status') == 'found':
                 self.recv_file(file)
-            else:
-                pkt.__print__()
-        except ConnectionError:
-            pass
 
     def recv_file(self, file):
         open(file=file, mode='wb').close()
         f = open(file=file, mode='ab')
         while True:
-            try:
-                res = self.socket.recv(PACKET_SIZE)
-                if res:
-                    pkt = Packet(pickled=res)
-                    pkt.__print__()
-                    # Check for corruption
-                    if pkt.__validate__():
-                        f.write(pkt.__get__('data'))
-                        # Send positive ack
-                        ack = Packet(seq_num=pkt.__get__('seq_num'), ack='+')
-                        self.socket.send(ack.__dumb__())
-                    else:  # Send negative ack
-                        ack = Packet(seq_num=pkt.__get__('seq_num'), ack='-')
-                        self.socket.send(ack.__dumb__())
-                else:
-                    raise ConnectionError
-            except ConnectionError:
+            res = self.socket.recv(PACKET_SIZE)
+            if not res:
+                print('Disconnected')
                 break
+
+            pkt = Packet(pickled=res)
+            pkt.__print__()
+
+            # Check for corruption
+            if pkt.__validate__():
+                f.write(pkt.__get__('data'))
+                # Send positive ack
+                ack = Packet(seq_num=pkt.__get__('seq_num'), ack='+')
+                self.socket.send(ack.__dumb__())
+            else:  # Send negative ack
+                ack = Packet(seq_num=pkt.__get__('seq_num'), ack='-')
+                self.socket.send(ack.__dumb__())
         f.close()
         self.socket.close()
 
--- stop-and-wait/Server.py
+++ stop-and-wait/Server.py
@@ -1,4 +1,5 @@
 import os
+from _socket import timeout
 from threading import Thread
 
 from Packet import Packet
@@ -19,8 +20,8 @@ class Server:
         print('[Listening on ]', self.address)
         while True:
             client, address = self.socket.accept()
-            print('Got Connection', client, address)
-            client.settimeout(100)
+            print('Client connected, address: ', address)
+            client.settimeout(4)  # Client Timeout in sec
 
             # create new thread to serve the client
             thread = Thread(target=self.serve_client, args=(client, address))
@@ -38,8 +39,6 @@ class Server:
         file = SERVER_FOLDER + pkt.__get__('file')
         if os.path.isfile(file):  # if file found on the server
             pkt = Packet(status='found')
-            pkt.__print__()
-
             client.send(pkt.__dumb__())
 
             # Logic goes here
@@ -47,22 +46,35 @@ class Server:
             f = open(file=file, mode='rb')
             data = f.read(CHUNK_SIZE)
             while data:
-                pkt = Packet(data=data, seq_num=seq_num)
-                client.send(pkt.__dumb__())
 
-                # w8 for Ack
-                res = client.recv(PACKET_SIZE)
-                pkt = Packet(pickled=res)
-                pkt.__print__()
+                # Send Packet and block until pos ack is received
+                while True:
+                    pkt = Packet(data=data, seq_num=seq_num)
+                    client.send(pkt.__dumb__())
+
+                    try:  # Wait for response or timeout
+                        res = client.recv(PACKET_SIZE)
+                        if not res:
+                            print('Client Disconnected')
+                            break
+
+                        pkt = Packet(pickled=res)
+                        pkt.__print__()
+                        if pkt.__get__('ack') == '+':
+                            break
+                        else:
+                            print('Negative ack, resending : ' + str(seq_num))
+                    except timeout:
+                        print('Timeout, resending packet: ' + str(seq_num))
 
                 seq_num += 1
                 data = f.read(CHUNK_SIZE)
 
         else:  # if file not found, send not found packet
             pkt = Packet(status='not_found')
-            pkt.__print__()
             client.send(pkt.__dumb__())
 
+        print('Client disconnected, address: ', address)
         client.close()
 
 
