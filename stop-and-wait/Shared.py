e5481dcec1e8a4ece7b420ba280289087efc9e4c
Ahmed Lotfy <A7med.lotfey@gmail.com>
2018-05-03 02:20:53 +0200

    Read args from file.

--- stop-and-wait/Shared.py
+++ stop-and-wait/Shared.py
@@ -1,20 +1,32 @@
 import socket
 
-SERVER_PORT = 9999
-SERVER_LOCAL_ADDRESS = 'localhost'  # local_address(SERVER_PORT)
-SERVER_FOLDER = 'server/'
 
-CLIENT_FOLDER = 'client/'
-CLIENT_TIMEOUT_TRIALS = 3
-
-LOSS_PROBABILITY = 0.5
-PACKET_SIZE = 3 * 1024
-CHUNK_SIZE = 2 * 1024
-
-
-def local_address(port):
+def get_local_address(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("192.168.1.1", 80))
     address = (s.getsockname()[0], port)
     s.close()
     return address
+
+
+def read_args():
+    args = {}
+    with open("input.txt") as input:
+        for line in input:
+            (key, val) = line.strip().split('=')
+            args[key] = val
+    return args
+
+
+args = read_args()
+SERVER_PORT = int(args['SERVER_PORT'])
+SERVER_ADDRESS = args['SERVER_ADDRESS']  # get_local_address(SERVER_PORT)
+LOSS_PROBABILITY = int(args['LOSS_PROBABILITY'])
+CORRUPTION_PROBABILITY = int(args['CORRUPTION_PROBABILITY'])
+
+SERVER_FOLDER = 'server/'
+CLIENT_FOLDER = 'client/'
+CLIENT_TIMEOUT_TRIALS = 3
+
+PACKET_SIZE = 3 * 1024
+CHUNK_SIZE = 2 * 1024
