174b10aea6acb804bf8e072500200e5e70863f9a
Ahmed Lotfy <A7med.lotfey@gmail.com>
2018-05-05 04:07:58 +0200

    Packet refactored.

--- go-back-n/Packet.py
+++ go-back-n/Packet.py
@@ -1,52 +1,89 @@
-import pickle
-import pprint
 from hashlib import sha1
 from termcolor import colored
 
+from Shared import *
+
 
 class Packet:
-    def __init__(self, pickled=None, seq_num=0, data=b'',
+    def __init__(self, res=None, seq_num=0, data=b'',
                  ack='', file='', status=''):
-        if pickled is not None:
-            self.packet = pickle.loads(pickled)
+        self.keys = [
+            "status",
+            "file",
+            "ack",  # 1 byte
+            "seq_num",  # 8 bytes
+            "checksum",  # 40 bytes
+            "data"  # 1900 bytes
+        ]
+        if res is not None:
+            self.packet = self.__load__(res)
         else:
             self.packet = {
-                "status": status,
-                "file": file,
-                "ack": ack,
-                "seq_num": seq_num,
-                "checksum": sha1(data).hexdigest() if data else '',
-                "data": data
+                "status": status.encode(),
+                "file": file.encode(),
+                "ack": ack.encode(),  # 1 byte
+                "seq_num": int(seq_num).to_bytes(8, byteorder='little',
+                                                 signed=True),  # 8 bytes
+                "checksum": sha1(data).hexdigest().encode(),  # 40 bytes
+                "data": data if data else b'',  # 1939 bytes
             }
 
-    def __dumb__(self):
-        return pickle.dumps(self.packet)
+        dummy_bytes = 2000
+        for val in self.packet.values():
+            dummy_bytes -= len(val)
+        self.packet['dummy'] = b'x' * dummy_bytes
+
+    def __dump__(self):
+        return DELIMITER.join(self.packet.values())
+
+    def __load__(self, res):
+        packet = {}
+        values = res.split(DELIMITER)
+        for key, val in zip(self.keys, values):
+            packet[key] = val
+        return packet
 
     def __validate__(self):
-        return self.packet['checksum'] == sha1(self.packet['data']).hexdigest()
+        return self.packet['checksum'] == sha1(
+            self.packet['data']).hexdigest().encode()
 
     def __get__(self, field):
         if field == 'seq_num':
-            return str(self.packet[field])
-        else:
+            return str(int.from_bytes((self.packet[field]),
+                                      byteorder='little',
+                                      signed=True))
+        elif field == 'data':
             return self.packet[field]
+        else:
+            return self.packet[field].decode()
 
-    def __print__(self):
+    def __print__(self, from_address='', to_address=''):
+        from_address = str(from_address)
+        to_address = str(to_address)
+
         status = self.__get__('status')
         file = self.__get__('file')
         ack = self.__get__('ack')
-        seq_num = str(self.__get__('seq_num'))
+        seq_num = self.__get__('seq_num')
 
         if ack == '+':
-            print(colored('Positive Ack ' + seq_num, color='green'))
+            print(colored('Positive Ack ' + seq_num +
+                          (' => from ' + from_address if from_address else ''),
+                          color='green'))
         elif ack == '-':
-            print(colored('Negative Ack ' + seq_num, color='red'))
+            print(colored('Negative Ack ' + seq_num +
+                          (' => from ' + from_address if from_address else ''),
+                          color='red'))
         elif file:
-            print(colored('Requesting file: ' + file, color='cyan'))
+            print(colored('Requesting file: ' + file,
+                          color='cyan'))
         elif status == 'found':
-            print(colored('File is found, recieving', color='green'))
+            print(colored('File is found, recieving from server',
+                          color='green'))
         elif status == 'not_found':
-            print(colored('File not found', color='red'))
+            print(colored('File not found',
+                          color='red'))
         else:
-            print(colored('Recieved Packet ' + seq_num, color='yellow'))
-        # pprint.pprint(self.packet)
+            print(colored('Packet ' + seq_num +
+                          (' => to ' + to_address if to_address else ''),
+                          color='yellow'))
